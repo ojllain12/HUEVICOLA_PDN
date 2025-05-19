80ee512612d065b0464c5fd868a6eb446d096411
Nikoxz27 <nikolasxz27@gmail.com>
2025-05-18 19:07:47 -0500

    INICIAL
    - Se agregan los proyectos de base

---
+++ Huevicola-Spring/src/main/java/com/ufpso/Huevicola/HuevicolaApplication.java
@@ -0,0 +1,13 @@
+package com.ufpso.Huevicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuevicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuevicolaApplication.class, args);
+	}
+
+}
---
+++ Huevicola-Spring/src/test/java/com/ufpso/Huevicola/HuevicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpso.Huevicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuevicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
